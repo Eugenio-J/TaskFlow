32b14dcfdfe19bd0d24ad63dfadbe1b0382730c2
Eugenio-J <jerichoeugenio2@gmail.com>
2026-02-13 22:39:14 +0800

    Remove .vs folder and update gitignore

--- backend/TaskFlow.API/obj/Debug/net10.0/TaskFlow.API.AssemblyInfo.cs
+++ backend/TaskFlow.API/obj/Debug/net10.0/TaskFlow.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskFlow.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+070603f465f33e6acdef06ff615fe522b1c0866e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskFlow.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskFlow.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
